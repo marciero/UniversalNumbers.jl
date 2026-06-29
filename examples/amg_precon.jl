1bbd5c45e9647af037b13a5b923a5532b1254c6a
jamesquinlan <james@stillwater-sc.com>
2026-06-29 14:45:12 -0400

    minor tweak
    Signed-off-by: jamesquinlan <james@stillwater-sc.com>

--- examples/amg_precon.jl
+++ examples/amg_precon.jl
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------
 # J.W. Ruge and K. Stuben, Algebraic multigrid, in Multigrid Methods, 
 #   vol. 3 of Frontiers in Applied Mathematics, SIAM, Philadelphia, PA, 
-#   1987, pp. 73â€“130.  
+#   1987, pp. 73--130.  
 # ---------------------------------------------------------------------
 
 using UniversalNumbers
